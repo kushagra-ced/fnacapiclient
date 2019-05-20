f46e92930e6b0dfb354a72adfd2c1352a85f1d84
Aditi Singh <aditisingh@cedcoss.com>
2019-05-20 15:56:50 +0530

    Update OffersQueryModel.php

--- src/FnacApiGui/src/FnacApiGui/Model/OffersQueryModel.php
+++ src/FnacApiGui/src/FnacApiGui/Model/OffersQueryModel.php
@@ -68,20 +68,16 @@ class OffersQueryModel extends Model
   public function updateOffer($client, $options)
   {    
     extract($options);
-   
     $offer = new Offer();
-    $offer->setOfferReferenceType(OfferReferenceType::SELLER_SKU);
-    $offer->setOfferReference($offer_sku);
-    $offer->setQuantity($quantity);
-    $offer->setPrice($price);
-    $offer->setDescription($description);
-
+      $offer->setOfferReferenceType($options['product_reference_type']);
+      $offer->setOfferReference($options['offre_reference']);
+      $offer->setQuantity($options['quantity']);
+      $offer->setPrice($options['price']['price']);
     $offerUpdate = new OfferUpdate();
     $offerUpdate->addOffer($offer);
 
     //Call the service
     $offerUpdateResponse = $client->callService($offerUpdate);
-        
     return $offerUpdateResponse;
   }
   
